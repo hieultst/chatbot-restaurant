ce07950c0131654481dd447ee51ab8d991f324fd
Hieult <infor.letrunghieu@gmail.com>
2022-09-30 11:37:40 +0700

    update

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -1,3 +1,5 @@
+import request from "request";
+
 require("dotenv").config();
 
 let getHomePage = (req, res) => {
@@ -11,10 +13,21 @@ let postWebhook = (req, res) => {
     if (body.object === "page") {
         // Iterates over each entry - there may be multiple if batched
         body.entry.forEach(function (entry) {
-            // Gets the message, entry.massaging is an array, but
-            // will only ever contain one massage, so we get index 0
-            let webhook_event = entry.massaging[0];
+            // Gets the body of the webhook event
+            let webhook_event = entry.messaging[0];
             console.log(webhook_event);
+
+            // Get the sender PSID
+            let sender_psid = webhook_event.sender.id;
+            console.log("Sender PSID: " + sender_psid);
+
+            // Check if the event is a message or postback and
+            // pass the event to the appropriate handler function
+            if (webhook_event.message) {
+                handleMessage(sender_psid, webhook_event.message);
+            } else if (webhook_event.postback) {
+                handlePostback(sender_psid, webhook_event.postback);
+            }
         });
         // Returns a '200 OK' response to all requests
         res.status(200).send("EVENT_RECEIVED");
@@ -46,6 +59,53 @@ let getWebhook = (req, res) => {
         }
     }
 };
+
+// Handles messages events
+function handleMessage(sender_psid, received_message) {
+    let response;
+
+    // Check if the message contains text
+    if (received_message.text) {
+        // Create the payload for a basic text message
+        response = {
+            text: `You sent the message: "${received_message.text}". Now send me an image!`,
+        };
+    }
+
+    // Sends the response message
+    callSendAPI(sender_psid, response);
+}
+
+// Handles messaging_postbacks events
+function handlePostback(sender_psid, received_postback) {
+    // Construct the message body
+    let request_body = {
+        recipient: {
+            id: sender_psid,
+        },
+        message: response,
+    };
+
+    // Send the HTTP request to the Messenger Platform
+    request(
+        {
+            uri: "https://graph.facebook.com/v2.6/me/messages",
+            qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
+            method: "POST",
+            json: request_body,
+        },
+        (err, res, body) => {
+            if (!err) {
+                console.log("message sent!");
+            } else {
+                console.error("Unable to send message:" + err);
+            }
+        }
+    );
+}
+
+// Sends response messages via the Send API
+function callSendAPI(sender_psid, response) {}
 
 module.exports = {
     getHomePage,
