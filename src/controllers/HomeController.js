3510726bc9eb88966b650f8e6b532449c26a1fb9
Hieult <infor.letrunghieu@gmail.com>
2022-09-30 13:48:56 +0700

    update get started

--- src/controllers/HomeController.js
+++ src/controllers/HomeController.js
@@ -121,6 +121,10 @@ function handlePostback(sender_psid, received_postback) {
         response = { text: "Thanks!" };
     } else if (payload === "no") {
         response = { text: "Oops, try sending another image." };
+    } else if (payload === "GET_STARTED") {
+        response = {
+            text: "OK. Welcome to our restaurant <3",
+        };
     }
     // Send the message to acknowledge the postback
     callSendAPI(sender_psid, response);
